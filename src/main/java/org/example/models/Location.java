20826d7c5968bbb148d33a0d82a9d64c1b321d97
christosste86 <christosste86@gmail.com>
2024-11-12 07:06:46 +0100

    add setter

--- src/main/java/org/example/models/Location.java
+++ src/main/java/org/example/models/Location.java
@@ -42,6 +42,10 @@ public class Location {
         return id;
     }
 
+    public String getVillage() {
+        return village;
+    }
+
     public void setId(long id) {
         this.id = id;
     }
