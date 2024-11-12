cd45ad0a3008e135834ab0c6945e4e24a18a41bb
I have an idea <49106207+christosste86@users.noreply.github.com>
2024-11-12 09:11:28 +0100

    add isDuplicateObjectInDB() and List<Location> filteredLocationList() to find if the db have the same location with current location.

--- src/main/java/org/example/services/LocationServices.java
+++ src/main/java/org/example/services/LocationServices.java
@@ -4,7 +4,6 @@ import org.example.connections.api.GeocodeService;
 import org.example.connections.db.daos.GenericDao;
 import org.example.models.Location;
 import org.example.connections.db.services.GenericService;
-import org.example.connections.photos.PhotoData;
 import org.example.models.Photo;
 
 import java.nio.file.Path;
@@ -13,40 +12,29 @@ import java.util.List;
 public class LocationServices {
     private GenericDao<Location, Long> locationDao = new GenericDao<>(Location.class);
     private GenericService<Location, Long> locationService = new GenericService<>(locationDao);
-    private Location locationObject = new Location();
-    private Photo photoObject = new Photo();
-
-    public LocationServices(Location location){
-    }
+    private Location locationObject;
+    private Photo photoObject;
 
     public LocationServices(Path photoPath) {
         PhotoService photoService = new PhotoService(photoPath);
         this.photoObject = photoService.getPhotoObject();
-        System.out.println(photoObject.getLatitude() +" "+ photoObject.getLongitude());
         GeocodeService geocodeService = new GeocodeService(photoObject.getLatitude(), photoObject.getLongitude());
-        this.locationObject = geocodeService.getLocation();
-        this.photoObject.setLocation(saveToDBAndGetObject());
-         //make condition if path exist
+        this.locationObject = geocodeService.getLocationObject();
     }
 
-    private boolean isDuplicateObject(double longitude, double latitude){
-        if(filteredLocationByLonAndLat(longitude, latitude).isEmpty()){
-            return false;
-        }else return true;
+    private boolean isDuplicateObjectInDB(){
+        return !filteredLocationList().isEmpty();
     }
 
-    private List<Location> filteredLocationByLonAndLat(double longitude, double latitude){
+    private List<Location> filteredLocationList(){
         return locationService.getAll()
                 .stream()
-                .filter(o-> o.getLongitude() == longitude && o.getLatitude() == latitude)
+                .filter(l-> l.getLatitude() == this.locationObject.getLatitude() && l.getLongitude() == this.locationObject.getLongitude())
                 .toList();
     }
 
-    public Location saveToDBAndGetObject(){
-        if (!isDuplicateObject(locationObject.getLongitude(), locationObject.getLatitude())){
-            locationService.save(this.locationObject);
-            return locationObject;
-        }else return filteredLocationByLonAndLat(locationObject.getLongitude(), locationObject.getLatitude()).get(0);
+    public void saveToDB(){
+        locationService.save(this.locationObject);
     }
 
     public void updateToDB(){
