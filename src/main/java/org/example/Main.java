8394be7083dbffe2726d90712cc295b98b6c1227
I have an idea <49106207+christosste86@users.noreply.github.com>
2024-11-14 15:00:12 +0100

    remove all data from main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,11 @@
 package org.example;
 
-import org.example.connections.FtpConnect;
-import org.example.connections.PhotoExplorer;
+import org.example.connections.FtpClient;
 import org.example.services.ModifyPhoto;
 
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-
-        //Ftp Connect
-        FtpConnect motorolaConnect = new FtpConnect("192.168.1.13",2221, "christos", "android");
-        motorolaConnect.connect();
-        ModifyPhoto ftpModifyPhoto = new ModifyPhoto();
-
-        Path destinationSource =  Path.of("U:\\BackUp\\Photos");
-        motorolaConnect.downloadFiles("/DCIM/Camera",destinationSource);
-        motorolaConnect.disconnect();
-
-
-
-
-
-
-//        LocationServices myPhotoLocation = new LocationServices(myPhotoPath);
     }
 }
