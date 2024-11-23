8591e5d36d0172a0550c3f11113e1fcb46ac5677
christosste86 <christosste86@gmail.com>
2024-11-23 08:26:34 +0100

    complete test

--- src/test/java/org/example/connections/api/GeocodeServiceTest.java
+++ src/test/java/org/example/connections/api/GeocodeServiceTest.java
@@ -26,22 +26,22 @@ class GeocodeServiceTest {
 
   @Test
   void locatedTest(){
-    //Latitude: 37.9703 Longitude: 23.7225
-    GeocodeService geocodeService = new GeocodeService(null, null);
-    assertNull(geocodeService.getLocationObject().getLatitude());
-    assertNull(geocodeService.getLocationObject().getLongitude());
-    assertNull(geocodeService.getLocationObject().getCountry_code());
-    assertNull(geocodeService.getLocationObject().getCountry());
-    assertNull(geocodeService.getLocationObject().getState());
-    assertNull(geocodeService.getLocationObject().getPostcode());
-    assertNull(geocodeService.getLocationObject().getCounty());
-    assertNull(geocodeService.getLocationObject().getMunicipality());
-    assertNull(geocodeService.getLocationObject().getCity());
+    GeocodeService geocodeService = new GeocodeService(37.969432299999994, 23.72236550000001);
+    assertEquals(37.969432299999994,geocodeService.getLocationObject().getLatitude());
+    assertEquals(23.72236550000001, geocodeService.getLocationObject().getLongitude());
+    assertEquals("gr",geocodeService.getLocationObject().getCountry_code());
+    assertEquals("Greece", geocodeService.getLocationObject().getCountry());
+    assertEquals("Attica", geocodeService.getLocationObject().getState());
+    assertEquals("117 42",geocodeService.getLocationObject().getPostcode());
+    assertEquals("Regional Unit of Central Athens",geocodeService.getLocationObject().getCounty());
+    assertEquals("Municipality of Athens",geocodeService.getLocationObject().getMunicipality());
+    assertEquals("Athens",geocodeService.getLocationObject().getCity());
     assertNull(geocodeService.getLocationObject().getVillage());
-    assertNull(geocodeService.getLocationObject().getSuburb());
-    assertNull(geocodeService.getLocationObject().getRoad());
-    assertNull(geocodeService.getLocationObject().getHouse_number());
+    assertEquals("Πετράλωνα", geocodeService.getLocationObject().getSuburb());
+    assertEquals("Ροβέρτου Γκάλλι", geocodeService.getLocationObject().getRoad());
+    assertEquals("43", geocodeService.getLocationObject().getHouse_number());
     assertNull(geocodeService.getLocationObject().getShop());
     assertNull(geocodeService.getLocationObject().getTourism());
+
   }
 }
