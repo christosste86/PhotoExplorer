0eaa480cc53dd116a39458c0209cd15277acdb1b
christosste86 <christosste86@gmail.com>
2024-11-23 07:28:09 +0100

    prepare test

--- src/test/java/org/example/connections/api/GeocodeServiceTest.java
+++ src/test/java/org/example/connections/api/GeocodeServiceTest.java
@@ -23,4 +23,25 @@ class GeocodeServiceTest {
       assertNull(geocodeService.getLocationObject().getShop());
       assertNull(geocodeService.getLocationObject().getTourism());
   }
+
+  @Test
+  void locatedTest(){
+    //Latitude: 37.9703 Longitude: 23.7225
+    GeocodeService geocodeService = new GeocodeService(null, null);
+    assertNull(geocodeService.getLocationObject().getLatitude());
+    assertNull(geocodeService.getLocationObject().getLongitude());
+    assertNull(geocodeService.getLocationObject().getCountry_code());
+    assertNull(geocodeService.getLocationObject().getCountry());
+    assertNull(geocodeService.getLocationObject().getState());
+    assertNull(geocodeService.getLocationObject().getPostcode());
+    assertNull(geocodeService.getLocationObject().getCounty());
+    assertNull(geocodeService.getLocationObject().getMunicipality());
+    assertNull(geocodeService.getLocationObject().getCity());
+    assertNull(geocodeService.getLocationObject().getVillage());
+    assertNull(geocodeService.getLocationObject().getSuburb());
+    assertNull(geocodeService.getLocationObject().getRoad());
+    assertNull(geocodeService.getLocationObject().getHouse_number());
+    assertNull(geocodeService.getLocationObject().getShop());
+    assertNull(geocodeService.getLocationObject().getTourism());
+  }
 }
